c8f5272495435bdce04478298fec8ce1a901886c
Orhun Çimen <orhun.horhor@gmail.com>
2025-03-11 14:00:34 +0300

    timeout4

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -22,7 +22,7 @@ public class LoginPage extends BasePage {
         page.fill(passwordInput, password);
         page.click(loginButton);
         page.waitForTimeout(1);
-        page.waitForTimeout(2);
+        page.waitForTimeout(4);
     }
 
     public String getErrorMessage() {
