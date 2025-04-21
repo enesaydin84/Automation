61729f40e40acdc7928738c5747258e8fb3ebe89
Enes AYDIN <enesaydin4520@gmail.com>
2025-04-21 15:51:52 +0300

    Allure report out, Extentreport in :)

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,8 +1,13 @@
 package tests;
 
+import com.aventstack.extentreports.ExtentTest;
 import com.microsoft.playwright.*;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
+import utils.ExtentManager;
+
+import java.nio.file.Paths;
+import java.util.Base64;
 
 public class BaseTest {
     protected static Playwright playwright;
@@ -23,6 +28,7 @@ public class BaseTest {
         if (playwright != null) {
             playwright.close();
         }
+        ExtentManager.getInstance().flush();
     }
 
     @BeforeMethod
@@ -35,6 +41,11 @@ public class BaseTest {
 
     @AfterMethod
     public void closeContext(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            String screenshotPath = "test-output/screenshots/" + result.getName() + ".png";
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get(screenshotPath)));
+        }
+        
         try {
             Thread.sleep(3000); // Wait for 3 seconds after each test
         } catch (InterruptedException e) {
