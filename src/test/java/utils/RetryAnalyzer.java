d51834861cebfb4df5dac675ee91c9139f795514
abdulkadiraydin <abdulkadiraaydin@gmail.com>
2025-05-13 10:20:54 +0300

    Test Case Retry Yapısı

---
+++ src/test/java/utils/RetryAnalyzer.java
@@ -0,0 +1,19 @@
+package utils;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+
+    private int retryCount = 0;
+    private final int maxRetryCount = 2; // Maksimum tekrar sayısı
+
+    @Override
+    public boolean retry(ITestResult result) {
+        if (retryCount < maxRetryCount) {
+            retryCount++;
+            return true; // Test tekrar denenecek
+        }
+        return false; // Artık tekrar yok
+    }
+}
